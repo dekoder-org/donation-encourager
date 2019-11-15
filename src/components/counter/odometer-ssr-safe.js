700f1416f9bbc2a920e2bf72996c47aadbcf0187
Daniel Marcus <dm@dekoder.org>
2019-11-15 13:00:53 +0100

    odometer: format

--- src/components/counter/odometer-ssr-safe.js
+++ src/components/counter/odometer-ssr-safe.js
@@ -2,22 +2,22 @@ import React, { useEffect } from "react";
 // import Odometer from "react-odometerjs";
 
 // placeholder for SSR
-let Odometer = ({value}) => <div>{value}</div>;
+let Odometer = ({ value }) => <div>{value}</div>;
 
 let _odometer_import_triggered = false;
 
-const OdometerSsrSafe = (props) => {
+const OdometerSsrSafe = props => {
   useEffect(() => {
     if (_odometer_import_triggered) return;
     else {
       _odometer_import_triggered = true;
       // dynamic import of odometer
-      import('react-odometerjs').then(module => { 
+      import("react-odometerjs").then(module => {
         Odometer = module.default;
       });
     }
   }, []);
-  return <Odometer {...props} />
-}
+  return <Odometer {...props} />;
+};
 
 export default OdometerSsrSafe;
