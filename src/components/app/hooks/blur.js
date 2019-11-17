a8bb32598be1a944d3aa5c2eaad89fb536e2fd30
Daniel Marcus <dm@dekoder.org>
2019-11-17 21:15:39 +0100

    refactoring: blur

--- src/components/app/hooks/blur.js
+++ src/components/app/hooks/blur.js
@@ -13,23 +13,30 @@ const GRADIENT_CLASS_ACTIVE = "gradient-active";
 export default function useBlur({ blurEnabled }, targets) {
   const { boxesEnabled, wrapperClass } = useContext(Settings);
   const [blurActive, removeBlur] = useBlurState(blurEnabled, boxesEnabled);
-
   const blurEls = useChildReducer(targets, wrapperClass, blurElGetter);
   useClass(blurEls, true, BLUR_CLASS);
   useClass(blurEls, blurActive, BLUR_CLASS_ACTIVE);
-
   const gradientEls = useChildReducer(targets, wrapperClass, gradientElGetter);
   useClass(gradientEls, true, GRADIENT_CLASS);
   useClass(gradientEls, blurActive, GRADIENT_CLASS_ACTIVE);
-
   return [blurActive, removeBlur];
 }
+
 function useBlurState(blurEnabled, boxesEnabled) {
   const isInitiallyActive = blurEnabled && boxesEnabled;
   const [blurActive, setBlurActive] = useState(isInitiallyActive);
   useEffect(() => setBlurActive(isInitiallyActive), [isInitiallyActive]);
   const removeBlur = () => setBlurActive(false);
   return [blurActive, removeBlur];
+}
+
+function useClass(elements, active, className) {
+  useEffect(() => {
+    elements.forEach(el => (active ? el.classList.add(className) : ""));
+    return () => {
+      elements.forEach(el => el.classList.remove(className));
+    };
+  }, [elements, active, className]);
 }
 
 function blurElGetter(acc, allChildren, firstBoxIndex, wrapperClass) {
@@ -43,17 +50,6 @@ function gradientElGetter(acc, allChildren, firstBoxIndex) {
   const isFirstBoxLast = firstBoxIndex === allChildren.length - 1;
   const lastBefore = allChildren[firstBoxIndex - 1];
   return lastBefore && !isFirstBoxLast ? [...acc, lastBefore] : acc;
-}
-
-function useClass(elements, active, className) {
-  useEffect(() => {
-    if (active) {
-      elements.forEach(el => el.classList.add(className));
-    }
-    return () => {
-      elements.forEach(el => el.classList.remove(className));
-    };
-  }, [elements, active, className]);
 }
 
 function useChildReducer(targetElements, wrapperClass, reducer) {
